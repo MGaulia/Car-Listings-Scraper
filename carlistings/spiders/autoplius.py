162dcb9524031ea8b8ce13e9c2e68052b90f84c9
Matas <matas.gaulia@gmail.com>
2021-11-17 23:56:33 +0200

    first commit

--- carlistings/spiders/autoplius.py
+++ carlistings/spiders/autoplius.py
@@ -10,13 +10,38 @@ class AutopliusSpider(scrapy.Spider):
 
     def parse(self, response):
         headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+
+        #yield scrapy.Request("https://autoplius.lt/skelbimai/audi-a8-3-0-l-sedanas-2020-dyzelinas-17591653.html", callback=self.parsecar, meta={'download_timeout': 2}, headers = headers)
         for carlink in response.xpath('//a[@class="announcement-item"]/@href').extract():
             yield scrapy.Request(carlink, callback=self.parsecar, meta={'download_timeout': 2}, headers = headers)
 
     def parsecar(self, response):
         labels = [i.strip() for i in response.xpath('//div[@class="parameter-label"]//text()').extract()]
         values = [i.strip() for i in response.xpath('//div[@class="parameter-value "]//text()').extract()]
-        cardict = {}
+
+
+        cardict = {
+            "Pagaminimo data": None,
+            "Rida": None,
+            "Variklis": None,
+            "Kuro tipas": None,
+            "Kėbulo tipas": None,
+            "Durų skaičius": None,
+            "Varantieji ratai": None,
+            "Pavarų dėžė": None,
+            "Klimato valdymas": None,
+            "Spalva": None,
+            "Vairo padėtis": None,
+            "Ratlankių skersmuo": None,
+            "Nuosava masė, kg": None,
+            "Sėdimų vietų skaičius": None,
+            "Pirmosios registracijos šalis": None,
+            "Kėbulo numeris (VIN)": None,
+            "SDK": None,
+            "Euro standartas": None,
+            "CO₂ emisija, g/km": None,
+            "Taršos mokestis": None
+        }
         for  label, value in zip(labels, values):
             cardict[label] = value
         yield cardict
